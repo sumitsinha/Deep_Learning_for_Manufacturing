83724b93f271c1717473e65c971f207288b53ab8
Sumit Sinha <Sinha_S>
2019-12-05 16:34:29 +0000

    Setup Support added for pip

--- setup.py
+++ setup.py
@@ -4,24 +4,18 @@ import sys
 
 _here = os.path.abspath(os.path.dirname(__file__))
 
-
-if sys.version_info[0] < 3:
-    with open(os.path.join(_here, 'README.md')) as f:
-        long_description = f.read()
-else:
-    with open(os.path.join(_here, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(_here, 'README.md'), encoding='utf-8') as f:
         long_description = f.read()
 
 version = {}
-with open(os.path.join(_here, 'dlmf', 'version.py')) as f:
+with open(os.path.join(_here, 'version.py')) as f:
     exec(f.read(), version)
 
-with open('../requirements.txt') as f:
+with open('requirements.txt') as f:
     required = f.read().splitlines()
-#requirements = ["ipython>=6"]
 
 setup(
-    name="deep-learning-for-manufacturing",
+    name="dlmfg",
     version=version['__version__'],
     author="Sumit Sinha",
     author_email="sumit.sinha.1@warwick.ac.uk",
@@ -31,13 +25,14 @@ setup(
     url="https://github.com/sumitsinha/Deep_Learning_for_Manufacturing",
     packages=find_packages(),
     license='MIT',
-    packages=['dlmfg'],
     include_package_data=True,
     install_requires=required,
     classifiers=[
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
     ],
 )

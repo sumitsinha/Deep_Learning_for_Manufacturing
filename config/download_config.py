c415fdd49d252327e58b6b4f979bdb1cda6251e3
Sumit Sinha <Sinha_S>
2020-02-06 10:16:24 +0000

    Clean_up

--- config/download_config.py
+++ config/download_config.py
@@ -24,10 +24,10 @@
 	:param download_params['id_test_data_files_x']: List of test data files IDs consisting of x-deviation of nodes on the server, minimum one file id in the list is required
 	:type download_params['id_test_data_files_x']: list (required)
 
-	:param download_params['id_test_data_files_y']: List of test data files IDs consisting of y-deviation of nodes on the server, minimum one file id in the list is required
+	:param download_params['id_test_data_files_y']: List of test data files IDs consisting of y-deviation of nodes on the serve, minimum one file id in the list is required
 	:type download_params['id_test_data_files_y']: list (required)
 
-	:param download_params['id_test_data_files_z']: List of test data files IDs consisting of z-deviation of nodes on the server, minimum one file id in the list is required
+	:param download_params['id_test_data_files_z']: List of test data files IDs consisting of z-deviation of nodes on the serve, minimum one file id in the list is required
 	:type download_params['id_test_data_files_z']: list (required)
 
 	:param download_params['id_mapping_index']: Mapping File ID
@@ -40,14 +40,15 @@
 download_params={
 	   'download_type':'google_drive',
 	   'base_url':'https://drive.google.com/uc?id=',
-	   'id_kcc_files':['1yjDnSjyjw6-RYcrb7xtpedwnvMzAOo9V'],
-	   'id_test_kcc_files':['1yjDnSjyjw6-RYcrb7xtpedwnvMzAOo9V'],
-	   'id_data_files_x':['14NvGKB2eYLagkINPaMqio4ylxmK2qj4L'],
-	   'id_data_files_y':['1sUfusVW7119DgdlZylZH2jEyBXXtVVcs'],
-	   'id_data_files_z':['1MHhk9Xn7r7S0_PbA-QAKlEp5n0tFfKl_'],
-	   'id_test_data_files_x':['1--nXi2N2cFpF_mXirqUrxzfBCWMy537h'],
-	   'id_test_data_files_y':['1sUfusVW7119DgdlZylZH2jEyBXXtVVcs'],
-	   'id_test_data_files_z':['1MHhk9Xn7r7S0_PbA-QAKlEp5n0tFfKl_'],
-	   'id_mapping_index':'1yELJOyzgDOsrP5pP6xAy-LifC7gd2aqb',
-	   'id_nominal_cop':'1m2FWTnZ70_fftrG-APs9DZR-NuC73AQW'
+	   'id_kcc_files':['1MjUef2sgOA0aLrK-2yp0O8tMUYme9ksb'],
+	   'id_test_kcc_files':['1RpD1fDkEt3M9Gxl1XWg-Mi37MW2Bxx0x'],
+	   'id_data_files_x':['1lxNnDQF77FGquQJlGhfFGu-J6XNhae7_'],
+	   'id_data_files_y':['1Xje2PLy7d4BmPQr4K1xo8dV0fgZOXHAP'],
+	   'id_data_files_z':['1kWXaBJ283ifdmW0BuWh8wtAvAtfaYT-R'],
+	   'id_test_data_files_x':['19bcf6AqqR_95NZFGd9QjgqtPAS1DFNhK'],
+	   'id_test_data_files_y':['1DadKeGZ0C0vDH0qW4RZ4hWkREDCJLKEe'],
+	   'id_test_data_files_z':['1YENg66GhvFaz8G4lRBUO6r7IL13fMz-d'],
+	   'id_mapping_index':'1RibrkROYqHLRVljSGfe5jVIhpv_H9abP',
+	   'id_nominal_cop':'1hrTlEnHXHg7T53XvXEjhIPSrRSRJLqoo'
 }
+
